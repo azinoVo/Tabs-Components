ff4b7f34dd071295c261fbbaaf9f5ef6c8c37620
azinoVo <azinovo22@gmail.com>
2019-04-11 16:22:05 -0500

    Tabs Testing, add expl for new Dropdown

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -2,7 +2,7 @@ class Dropdown {
   constructor(element) {
     
     // Assign this.element to the dropdown element
-    this.element;
+    this.element = element;
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector();
@@ -26,3 +26,6 @@ class Dropdown {
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+// Create a variable called dropdowns and assign it to: the DOM representation of all classes with a class
+// name of dropdown. For each index within the nodeList of the DOM representation, create a new instance
+// of the Dropdown class with information from the DOM node.
